611bd0c031e941cf4f55286b254f4b00ef871f2e
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-03-05 20:48:01 +0530

    recording

--- src/main/java/hello/HelloApplication.java
+++ src/main/java/hello/HelloApplication.java
@@ -2,16 +2,25 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @SpringBootApplication
 public class HelloApplication {
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+	
 	
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello Openshift from HCL";
+		return "Hello Openshift!!";
 	}
 
 	public static void main(String[] args) {
