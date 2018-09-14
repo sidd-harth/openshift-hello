c8bdca7977bfac8cd4e2f704a0adac1967d0510a
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-09-14 10:26:11 +0530

    1026

--- src/main/java/hello/HelloApplication.java
+++ src/main/java/hello/HelloApplication.java
@@ -54,6 +54,6 @@ public class HelloApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(HelloApplication.class, args); 
 	}
 }
