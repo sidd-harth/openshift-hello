fc5998d0128aae71010e84f4095175c4f22b12a7
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2018-09-14 10:15:30 +0530

    Update HelloApplication.java

--- src/main/java/hello/HelloApplication.java
+++ src/main/java/hello/HelloApplication.java
@@ -7,6 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 
 @RestController
@@ -22,6 +29,32 @@ public class HelloApplication {
 	public String hello() {
 		return "Hello Openshift from HCL!!!!";
 	}
+	
+	
+	
+	@Value("${other.openshift.app.service.url:http://browser-service:8080}")
+        private String remoteURL;
+	
+	@RequestMapping("/other-service")
+        public ResponseEntity<String> getCustomer(@RequestHeader("User-Agent") String userAgent) {
+        try {
+            /**
+             * Set baggage
+             */
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
+            String response = responseEntity.getBody();
+            return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+        } catch (HttpStatusCodeException ex) {
+           // logger.warn("Exception trying to get the response from preference service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT,
+                            String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
+        } catch (RestClientException ex) {
+            ///logger.warn("Exception trying to get the response from preference service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT, ex.getMessage()));
+        }
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
